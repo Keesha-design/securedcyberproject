d3719132660f7f3f0b037e73304198a1778a3142
Keesha-design <96183596+Keesha-design@users.noreply.github.com>
2024-01-13 07:37:19 +0100

    Update settings.py

--- todo_list_app/todo_list_app/settings.py
+++ todo_list_app/todo_list_app/settings.py
@@ -80,13 +80,14 @@ WSGI_APPLICATION = 'todo_list_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+if not DEBUG:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
